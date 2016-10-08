0c3985f03da5d55d524ee2faaf16eea5ffcd7103
Cherry Mae Pozon <cmpozon@gmail.com>
2016-10-08 08:45:45 +0800

    Add files via upload

---
+++ euler3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cmath>
+#include <fstream>
+using namespace std;
+
+int main(){
+	int i, num, sum=0;
+	ifstream f("input.txt");
+	FILE *fp;
+	fp = fopen ("input.txt", "r");
+	
+	if(!fp){
+		cout << "Cannot open file";
+		system ("pause");
+		exit(1);
+	}
+	while (f >> num){
+		
+	}
+	for (i=0; i < num; i++){
+		if (i%3==0 || i%5 ==0)
+		sum += i;
+		
+	}
+	cout << sum;
+	return 0;
+}
